20953f99a3c95bfb1e647dc7fe88df4c1d8e298c
KwonYunje <yunje0012@gmail.com>
2025-07-10 22:50:51 +0900

    장바구니 컨트롤러

---
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import com.example.demo.Service.CartService;
+import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.CartItemRepository;
+import com.example.demo.repository.ProductRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("customer/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(ProductRepository productRepository, CartItemRepository cartItemRepository, CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    //장바구니 조회
+    @GetMapping
+    public String viewCart(HttpSession httpSession, Model model) {
+        UserEntity user = (UserEntity) httpSession.getAttribute("loginUser");
+        List<?> cartItems = cartService.getCartItems(user, httpSession);
+        model.addAttribute("cartItems", cartItems);
+        return "customer/cart";
+    }
+
+    //장바구니에 물건 추가
+    @PostMapping("/add/{productId}")
+    public String addToCart(@PathVariable Long productId,
+                            @RequestParam(defaultValue = "1") int quantity,
+                            HttpSession httpSession) {
+        UserEntity user = (UserEntity) httpSession.getAttribute("loginUser");
+        cartService.addToCart(user, productId, quantity, httpSession);
+        return "redirect:/customer/cart";
+    }
+
+    //카트 비우기
+    @PostMapping("/clear")
+    public String clearCart(HttpSession httpSession) {
+        UserEntity user = (UserEntity) httpSession.getAttribute("loginUser");
+        cartService.clearCart(user, httpSession);
+        return "redirect:/customer/cart";
+    }
+}
