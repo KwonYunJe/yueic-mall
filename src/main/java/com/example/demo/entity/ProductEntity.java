d56081bbc30ef1a51d6d1c9f38401d38e65f9a61
KwonYunje <yunje0012@gmail.com>
2025-07-11 00:27:58 +0900

    물품 엔티티 썸네일 컬럼 추가

--- src/main/java/com/example/demo/entity/ProductEntity.java
+++ src/main/java/com/example/demo/entity/ProductEntity.java
@@ -22,6 +22,10 @@ public class ProductEntity {
     //가격
     private int price;
 
+    @Column(name = "thumbnail_url")  // 오타 수정도 포함!
+    private String thumbnailUrl;
+
+
     //판매자와의 관계 / 하나의 판매자 (UserEntity)가 여러 개의 상품(ProductEntity)를 가질 수 있음을 의미
     @ManyToOne
     //DB의 외래키 이름을 seller_id로 지정 (ProductEntity라는 테이블에 seller_id라는 외래키 컬럼이 생김
