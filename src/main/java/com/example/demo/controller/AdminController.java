d471ae84772730c010b5ebefeb43aa3147710788
KwonYunje <yunje0012@gmail.com>
2025-07-16 19:47:49 +0900

    이전의 모든 유저 비밀번화 일시 암호화
    이전의 모든 유저 비밀번화 일시 암호화

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.Service.UserService;
 import com.example.demo.entity.UserEntity;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/dashboard")
     public String showDashboard(HttpSession httpSession, Model model){
         UserEntity loginUser = (UserEntity) httpSession.getAttribute("loginUser");
@@ -21,5 +28,12 @@ public class AdminController {
 
         model.addAttribute("adminName", loginUser.getUsername());
         return "admin/dashboard";
+    }
+
+    //모든 유저 비밀번호 암호화
+    @GetMapping("/encode-passwords")
+    public String encodePasswords() {
+        userService.encodeAllPasswords();
+        return "redirect:/"; // 또는 결과 페이지
     }
 }
