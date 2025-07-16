8966b2ec12cadc1a2fdb3d408956113e0428e65f
KwonYunje <yunje0012@gmail.com>
2025-07-16 19:51:18 +0900

    상품 검색과 정렬을 위해 레포지토리 수정

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -9,4 +9,15 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     //판매자 이름으로 상품 가져오기
     List<ProductEntity> findBySellerId(Long sellerId);
+
+    //상품 등록일 순으로 정렬하여 가져오기
+    List<ProductEntity> findByNameContainingOrderByPriceAsc(String name);
+    List<ProductEntity> findByNameContainingOrderByPriceDesc(String name);
+    List<ProductEntity> findByNameContainingOrderByCreateTimeAsc(String name);
+    List<ProductEntity> findByNameContainingOrderByCreateTimeDesc(String name);
+
+    List<ProductEntity> findBySellerIdAndNameContainingOrderByPriceAsc(Long sellerId, String name);
+    List<ProductEntity> findBySellerIdAndNameContainingOrderByPriceDesc(Long sellerId, String name);
+    List<ProductEntity> findBySellerIdAndNameContainingOrderByCreateTimeAsc(Long sellerId, String name);
+    List<ProductEntity> findBySellerIdAndNameContainingOrderByCreateTimeDesc(Long sellerId, String name);
 }
